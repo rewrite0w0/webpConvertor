0a4059629e4787e65b7a94b42897c0b1a197d17c
oh taejun <rewrite0w0@gmail.com>
2022-09-29 10:14:25 +0900

    del: 주석삭제

--- index.py
+++ index.py
@@ -2,13 +2,8 @@ from PIL import Image
 
 print(Image)
 
-# image_old = "./src/page4/testResult_card-4.png"
-# image_new = "./src/page4/testResult_card-4.webp"
-
-
 page=""
-name="class1Laser"
-
+name=""
 
 image_o = f"./src/{page}/{name}.png"
 image_n = f"./src/{page}/{name}.webp"
